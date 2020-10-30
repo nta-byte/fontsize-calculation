8d5667c6f604d19b9db3db823a619e77cf7a3695
knight9fury@gmail.com <Oanh29011994>
2020-10-30 12:44:23 +0700

    update refactor code

--- cal.py
+++ cal.py
@@ -19,52 +19,40 @@ def get_img_paths(dir_, extensions=('.jpg', '.png', '.jpeg', '.PNG', '.JPG', '.J
     return img_paths
 
 
-def get_font_size2(text, wid, height, font_path=None):
+def get_font_size2(text, wid, height, font_path=None, fontsize=6, jumpsize=7, img_fraction=.3):
     if not font_path:
         font_path = "DejaVuSans.ttf"
-    img_fraction = .3
     photo = Image.new("1", (wid, height))
-    breakpoint = img_fraction * photo.size[0]
-    jumpsize = 7
-    fontsize = 6
-    font = ImageFont.FreeTypeFont(font_path, fontsize)
-    # print(font.getlength(text))
+    break_point = img_fraction * photo.size[0]
+    font_ = ImageFont.FreeTypeFont(font_path, fontsize)
     while True:
-        if font.getlength(text) < breakpoint:
+        if font_.getlength(text) < break_point:
             fontsize += jumpsize
         else:
             jumpsize = int(jumpsize / 2)
             fontsize -= jumpsize
-        font = ImageFont.FreeTypeFont(font_path, fontsize)
+        font_ = ImageFont.FreeTypeFont(font_path, fontsize)
         if jumpsize <= 1:
             break
-    fontsize = int(fontsize/img_fraction)
+    fontsize = int(fontsize / img_fraction)
     return fontsize
 
 
-img_dir = 'images'
-box_dir = 'GT_word_icdar_refined1508'
-
-images = get_img_paths(img_dir)
-
-for img_path in images:
-    img = cv2.imread(img_path)
-    label_path = os.path.join(box_dir, os.path.basename(os.path.splitext(img_path)[0]) + '.txt')
-    print(label_path)
-    data = pd.read_csv(label_path, usecols=np.r_[0:9], header=None)
+def calculate_mean_size_char(words, fontpath):
+    start = time.time()
+    previous_font_size = None
     wid_list = []
     hei_list = []
-    start = time.time()
-    for index, row in data.iterrows():
-        x0, y0, x1, y1, x2, y2, x3, y3, text = list(row)
-        wid = x1 - x0
-        hei = y3 - y0
-
-        font_size = get_font_size2(text, wid, hei)
-        # print(font_size, wid, hei, text)
-        font = ImageFont.truetype("DejaVuSans.ttf", font_size)
+    for word in words:
+        x, y, width, height, text = word
+        if previous_font_size is not None:
+            font_size = get_font_size2(text, width, height, font_path=fontpath, fontsize=previous_font_size, jumpsize=7,
+                                       img_fraction=1)
+        else:
+            font_size = get_font_size2(text, width, height, font_path=fontpath)
+            previous_font_size = font_size
+        font = ImageFont.truetype(fontpath, font_size)
         for c in text:
-            # print(c)
             wc, hc = font.getsize(c)
             wid_list.append(wc)
             hei_list.append(hc)
@@ -75,3 +63,26 @@ for img_path in images:
     stop = time.time()
     print('mean_wid:', mean_wid, '---mean_hei: ', mean_hei)
     print('processing time: ', stop - start)
+    return mean_wid, mean_hei
+
+
+if __name__ == '__main__':
+
+    img_dir = 'images'
+    box_dir = 'GT_word_icdar_refined1508'
+    font_path = "DejaVuSans.ttf"
+    images = get_img_paths(img_dir)
+
+    for img_path in images:
+        img = cv2.imread(img_path)
+        label_path = os.path.join(box_dir, os.path.basename(os.path.splitext(img_path)[0]) + '.txt')
+        print(label_path)
+        data = pd.read_csv(label_path, usecols=np.r_[0:9], header=None)
+        words_list = []
+        for index, row in data.iterrows():
+            x0, y0, x1, y1, x2, y2, x3, y3, text = list(row)
+            wid = x1 - x0
+            hei = y3 - y0
+            words_list.append([x0, y0, wid, hei, text])
+
+        calculate_mean_size_char(words_list, font_path)
